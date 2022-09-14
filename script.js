6f5d23f2bfdd90327dfa4dc8694f8b5900652c3c
FrenzH <franciattom99@gmail.com>
2022-09-14 14:22:30 +0100

    list-item

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const app= new Vue(
     {
         el:'#app',
+        data:{
+            
+            toDo:[
+                {toDoText:'Do Carousel',
+                 done: false},
+                {toDoText:'Do Carousel1',
+                 done: true},
+                {toDoText:'Do Carousel2',
+                 done: true},
+                {toDoText:'Do Carousel3',
+                 done: false}
+
+            ]
+
+        }
+            
         
     }
 ) 
